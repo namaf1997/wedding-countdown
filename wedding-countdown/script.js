5b34099d4801ffaf755b4d7401cbef6977dbea38
namaf1997 <kowloon2608@icloud.com>
2025-07-12 01:15:52 +0900

    Update script.js

--- wedding-countdown/script.js
+++ wedding-countdown/script.js
@@ -8,9 +8,7 @@ const diffDays = Math.max(0, Math.ceil((weddingDate - today) / (1000 * 60 * 60 *
 
 // 画像リスト（必要に応じて追加）
 const imageUrls = [
-  "https://cdn.glitch.global/3d474831-3b84-4b48-bc99-194ecc80d513/2021-12-25_01-14-54_153.jpeg?v=1751209059171",
-  "https://cdn.glitch.global/3d474831-3b84-4b48-bc99-194ecc80d513/250506_00034.JPEG?v=1751164276991",
-  "https://cdn.glitch.global/3d474831-3b84-4b48-bc99-194ecc80d513/2022-02-05_12-34-17_159.jpeg?v=1751209105828"
+  "2021-12-25_01-14-54_153.jpeg",
 ];
 
 const imageIndex = Math.min(imageUrls.length - 1, Math.max(0, imageUrls.length - diffDays));
